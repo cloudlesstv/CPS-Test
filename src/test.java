44a94fc0756cd88cd71d0c4a695a47e314a84130
Joel Obi <57414887+cloudlesstv@users.noreply.github.com>
2020-10-06 21:36:18 +0200

    Added debug commitment

--- src/test.java
+++ src/test.java
@@ -1,5 +1,9 @@
 public class test {
+
+    static String debug = new String("[Debug]");
+
     public static void main(String[] args) {
         new GUI();
+        System.out.println("Debug started");
     }
 }
