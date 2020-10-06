f6d9e5f65c02011c551a3ce64b57f753dd0df629
Joel Obi <57414887+cloudlesstv@users.noreply.github.com>
2020-10-06 18:39:49 +0200

    Darkmode-Toggle hinzugefügt

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,26 +7,38 @@ public class GUI implements ActionListener {
 
     int count;
     int cps;
+    Boolean darkmode = new Boolean(false);
 
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
     JLabel lblclicks = new JLabel("Clicks: " + count);
     JLabel lblcps = new JLabel("CPS: " + cps + " C/s");
-    JButton btnclick = new JButton("Klick mich");
+    JButton btnclick = new JButton("Click");
+    JButton btndarkmode = new JButton("Darkmode");
 
     Timer sec = new Timer(1000, this);
 
     public GUI() {
         frame.setVisible(true);
-        frame.setSize(500, 500);
+        frame.setSize(180, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.add(panel);
         panel.add(lblclicks);
         panel.add(lblcps);
         panel.add(btnclick);
+        panel.add(btndarkmode);
+
+        panel.setBackground(Color.WHITE);
+        btndarkmode.setBackground(Color.WHITE);
+        btnclick.setBackground(Color.WHITE);
+        btndarkmode.setForeground(Color.RED);
+        btnclick.setForeground(Color.BLACK);
+        lblclicks.setForeground(Color.BLACK);
+        lblcps.setForeground(Color.BLACK);
 
         btnclick.addActionListener(this);
+        btndarkmode.addActionListener(this);
     }
 
     @Override
@@ -38,9 +51,32 @@ public class GUI implements ActionListener {
             count++;
             lblclicks.setText("Clicks: " + count);
         }
+        
         if(e.getSource() == sec) {
             lblcps.setText("CPS: " + cps + " C/s");
             cps = 0;
+        }
+
+        if(e.getSource() == btndarkmode) {
+            if(darkmode == false) {
+                darkmode = true;
+                panel.setBackground(Color.BLACK);
+                btndarkmode.setBackground(Color.BLACK);
+                btnclick.setBackground(Color.BLACK);
+                btndarkmode.setForeground(Color.GREEN);
+                btnclick.setForeground(Color.WHITE);
+                lblclicks.setForeground(Color.WHITE);
+                lblcps.setForeground(Color.WHITE);
+            }else {
+                darkmode = false;
+                panel.setBackground(Color.WHITE);
+                btndarkmode.setBackground(Color.WHITE);
+                btnclick.setBackground(Color.WHITE);
+                btndarkmode.setForeground(Color.RED);
+                btnclick.setForeground(Color.BLACK);
+                lblclicks.setForeground(Color.BLACK);
+                lblcps.setForeground(Color.BLACK);
+            }
         }
     }
 }
