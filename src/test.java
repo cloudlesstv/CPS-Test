930b87afc4311b57a78c091da13475869671b06f
Joel Obi <57414887+cloudlesstv@users.noreply.github.com>
2020-10-07 20:44:26 +0200

    Made the Debug-Start more attractive

--- src/test.java
+++ src/test.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class test {
 
     static String debug = new String("[Debug]");
 
     public static void main(String[] args) {
-        new GUI();
+        System.out.println("");
+        System.out.println("Welcome to CPS-Test by CloudLessTv");
         System.out.println("Debug started");
+        System.out.println("");
+        new GUI();
     }
 }
