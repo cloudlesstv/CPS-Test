f1d3e0a84efeef8d3682730efa1e52b2b04a30ef
Joel Obi <57414887+cloudlesstv@users.noreply.github.com>
2020-10-10 12:55:15 +0200

    Organized everything

--- src/test.java
+++ src/test.java
@@ -1,8 +1,19 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.util.ArrayList;
 
 public class test {
 
+    static int count;
+    static int cps;
+    static int hcps;
+
+    static public ArrayList<Integer> stats = new ArrayList<Integer>();
+
+    static Boolean boolDouble = Boolean.FALSE;
+
     static String debug = new String("[Debug]");
+
+    static Timer sec = new Timer(1000, new ActionEventHandler());
 
     public static void main(String[] args) {
         System.out.println("");
