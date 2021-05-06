fd28d66d8d39f35cc2679b8a16a332a0c5d0ddef
Joel Obi <57414887+cloudlesstv@users.noreply.github.com>
2021-05-06 18:53:08 +0200

    Implemented CloudKey Function

--- src/ActionEventHandler.java
+++ src/ActionEventHandler.java
@@ -1,117 +1,85 @@
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class ActionEventHandler implements ActionListener, KeyListener, MouseListener {
-
-    Boolean boolSpace = new Boolean(Boolean.FALSE);
-
+public class ActionEventHandler implements ActionListener, MouseListener {
     @Override
     public void actionPerformed(ActionEvent e) {
+        RPC.update();
         if(e.getSource() == GUI.btnclick) {
+            DatabaseHandler.setTotal();
             if(!Main.sec.isRunning()) {
                 Main.sec.start();
-                System.out.println(Main.debug + "Timer Started");
+                Debug.printStacktrace("Timer started");
             }
             Main.cps++;
             Main.count++;
             if(Main.boolDouble == true) {
                 Main.cps++;
                 Main.count++;
-                System.out.println(Main.debug + "Clicks doubled.");
+                Debug.printStacktrace("Clicks doubled.");
             }
             GUI.lblclicks.setText("Clicks: " + Main.count);
-            System.out.println(Main.debug + "Click recognized. (#" + Main.count+ ")");
+            Debug.printStacktrace("Click recognized (#" + Main.count + ")");
+
         }
+
 
         if(e.getSource() == Main.sec) {
             GUI.lblcps.setText("CPS: " + Main.cps + " C/s");
             if(Main.cps > 0) {
                 GUI.lblcps.setText("CPS: " + Main.cps + " C/s");
-                System.out.println(Main.debug + "CPS Calculated(" + Main.cps + ")");
+                Debug.printStacktrace("CPS Calculated(" + Main.cps + ")");
                 Main.stats.add(Main.cps);
 
-                System.out.println(Main.debug + "Stats cached." + Main.stats);
+                Debug.printStacktrace("Stats cached." + Main.stats);
                 GUI.lbllast.setText("Last results: " + Main.stats);
                 for (int i = 0; i < Main.stats.size(); i++) {
                     if(i > 3) {
                         Main.stats.clear();
-                        System.out.println(Main.debug + "Last results cleared.");
+                        Debug.printStacktrace("Last results cleared.");
                         Main.stats.add(Main.cps);
                         GUI.lbllast.setText("Last results: " + Main.stats);
+
                     }
                 }
                 if (Main.cps > Main.hcps) {
-                    System.out.println(Main.debug + "New Highscore:" + Main.cps);
+                    Debug.printStacktrace("New Highscore:" + Main.cps);
                     Main.hcps = Main.cps;
                     GUI.lblHS.setText("Highscore: " + Main.hcps);
                 }
                 Main.cps = 0;
             }else {
                 GUI.lblcps.setText("CPS: " + Main.cps + " C/s");
-                System.out.println(Main.debug + "Timer stopped.");
+                Debug.printStacktrace("Timer stopped.");
                 Main.sec.stop();
             }
         }
 
-        if(e.getSource() == GUI.btndarkmode) {
-            if(GUI.boolDarkmode == false) {
-                GUI.boolDarkmode = true;
-                GUI.contentPanel.setBackground(Colors.Gunmetal);
-                GUI.btndarkmode.setBackground(Colors.Bdazzled_Blue);
-                GUI.btnclick.setBackground(Colors.Bdazzled_Blue);
-                GUI.btndarkmode.setForeground(Colors.Android_Green);
-                GUI.btnclick.setForeground(Colors.Cultured);
-                GUI.lblclicks.setForeground(Colors.Pale_Cerulean);
-                GUI.lblcps.setForeground(Colors.Pale_Cerulean);
-                GUI.lbllast.setForeground(Colors.Pale_Cerulean);
-                GUI.lblHS.setForeground(Colors.Pale_Cerulean);
+        if(e.getSource() == GUI.btnSettings) {
+            new Settings();
+        }else {
 
-                //Login
-                GUI.btnlogin.setBackground(Colors.Bdazzled_Blue);
-                GUI.btnlogin.setForeground(Colors.Cultured);
-                //Login
-                System.out.println(Main.debug + "Enabled Darkmode");
-            }else {
-                GUI.boolDarkmode = false;
-                GUI.contentPanel.setBackground(Colors.Gainsboro);
-                GUI.btndarkmode.setBackground(Colors.Light_Cyan);
-                GUI.btnclick.setBackground(Colors.Light_Cyan);
-                GUI.btndarkmode.setForeground(Colors.Imperial_Red);
-                GUI.btnclick.setForeground(Colors.Rich_Black_FOGRA_39);
-                GUI.lblclicks.setForeground(Colors.Charcoal);
-                GUI.lblcps.setForeground(Colors.Charcoal);
-                GUI.lbllast.setForeground(Colors.Charcoal);
-                GUI.lblHS.setForeground(Colors.Charcoal);
-                //Login
-                GUI.btnlogin.setBackground(Colors.Light_Cyan);
-                GUI.btnlogin.setForeground(Colors.Rich_Black_FOGRA_39);
-                //Login
-                System.out.println(Main.debug + "Disabled Darkmode");
-            }
-        }
-        if(e.getSource() == GUI.btnlogin) {
-            LoginForm.GUI();
         }
     }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        DatabaseHandler.setTotal();
+        if(!Main.sec.isRunning()) {
+            Main.sec.start();
+            Debug.printStacktrace("Timer started");
+        }
+        Main.cps++;
+        Main.count++;
+        if(Main.boolDouble) {
+            Main.cps++;
+            Main.count++;
+            Debug.printStacktrace("Clicks doubled.");
+        }
+        GUI.lblclicks.setText("Clicks: " + Main.count);
+        Debug.printStacktrace("Click recognized (#" + Main.count + ")");
 
     }
 
